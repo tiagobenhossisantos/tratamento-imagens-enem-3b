996e3cd9e0214cc986b4f04b0cb6a2de8d05f4b9
tiagobenhossisantos <tiago.benhossi.santos@escola.pr.gov.br>
2026-06-12 15:34:41 -0300

    nomes tratados

--- 9Passo-TratarNomes/renomear-outras.py
+++ 9Passo-TratarNomes/renomear-outras.py
@@ -12,7 +12,7 @@ OBS4: ENTENDA muito bem o for da linha 25!!!!!!!!!!!
 import os
 
 def renomear_questoes_simples():
-    pasta = "37-76"
+    pasta = "80-90"
     
     if not os.path.exists(pasta):
         print(f"Pasta {pasta} não encontrada!")
@@ -22,11 +22,11 @@ def renomear_questoes_simples():
     mapeamento = {}
         
     # Exemplo: parte_00x a parte_00y -> questao-x a questao-y
-    for i in range(41, 80+1):    # atualize seu for com o número da primeira imagem "parte_AlgumaCoisa.png" até o número da última imagem "parte_AlgumaCoisa.png" mais 1 da pasta
+    for i in range(81, 91+1):    # atualize seu for com o número da primeira imagem "parte_AlgumaCoisa.png" até o número da última imagem "parte_AlgumaCoisa.png" mais 1 da pasta
         antigo = f"parte_{i:03d}.png"
         #novo = f"questao-{i-6}-espanhol.png"  # faça uma conta: se a primeira pagina for 
         #novo = f"questao-{i-1}-ingles.png"
-        novo = f"questao-{i-4}.png" # faça uma conta: se o i do teu for está em 2, e precisa virar questão 35, como você transforma 2 em 35? faça a conta e coloque dentro da concatenação
+        novo = f"questao-{i-1}.png" # faça uma conta: se o i do teu for está em 2, e precisa virar questão 35, como você transforma 2 em 35? faça a conta e coloque dentro da concatenação
         
         mapeamento[antigo] = novo
     
